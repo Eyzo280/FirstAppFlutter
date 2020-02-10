c0adb0e1ac84821b4c5c86d0d69c7ae8baaa38eb
Eyzo280 <48450982+Eyzo280@users.noreply.github.com>
2020-02-10 18:57:02 +0100

    Anonymous Functions

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,37 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Anwer Chosen!!!');
+  }
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: 'My first app',),
-    ),);
+    var questions = ['Pytanie pierwsze', 'Pytanie drugie', 'Pytanie trzecie'];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app'),
+        ),
+        body: Column(
+          children: [
+            Text('Pytania'),
+            RaisedButton(
+              child: Text('Przycisk 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Przycisk 2'),
+              onPressed: () => print('Answer Chosen 2'),
+            ),
+            RaisedButton(
+              child: Text('Przycisk 3'),
+              onPressed: () {
+                print('Answer Chosen 3');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
